f1a36db22c122e65eef4d7bbeec6324b422bd425
Ngo_Quang_Minh <quangminhngo3@gmail.com>
2022-07-10 14:28:22 +0700

    Update 3: Food comment

--- main/java/Food.java
+++ main/java/Food.java
@@ -1,26 +1,25 @@
 
 public class Food extends VietNamTourism{
-	private String name = "Food";
 	private String type = "?s dbp:type ?type.\r\n";
 	private String region =	"?s dbp:region ?region.\r\n";
 	private String ingredient = "?s dbo:ingredient ?ingredient.\r\n";
 	private String mainIngredient = "?s dbp:mainIngredient ?mainIngredient.";
-	private String food = "{?s rdf:type dbo:Food.} UNION {?s dct:subject dbc:Vietnamese_cuisine} UNION {?s rdf:type yago:WikicatVietnameseRiceDishes} UNION {?s rdf:type yago:WikicatVietnameseBeverages}";
+	private String comment = "?s rdfs:comment ?comment";
+	// private String food = "{?s rdf:type dbo:Food.} UNION {?s dct:subject dbc:Vietnamese_cuisine} UNION {?s rdf:type yago:WikicatVietnameseRiceDishes} UNION {?s rdf:type yago:WikicatVietnameseBeverages}";
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 	public Food() {
 		super();
-		compulsory.add(food);
+		name = "Food";
+		constrain = "{{?s dbo:country dbr:Vietnam.} UNION {?s dbp:country dbr:Vietnam.}. ?s rdf:type dbo:Food.}\r\n"
+		+" UNION {?s dct:subject dbc:Vietnamese_cuisine} \r\n"
+		+" UNION {?s rdf:type yago:WikicatVietnameseRiceDishes}\r\n"
+		+" UNION {?s rdf:type yago:WikicatVietnameseBeverages}\r\n";
 		optional.add(type);
 		optional.add(region);
 		optional.add(ingredient);
 		optional.add(mainIngredient);
+		optional.add(comment);
 	}
 	
 }
